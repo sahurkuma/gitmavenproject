df5c029ed654f351ef03fb147574d8be8cc6e443
neeleshgoyal <goyalneelesh8@gmail.com>
2020-01-24 23:28:09 +0530

    first commit

---
+++ src/main/java/com/qa/actitime/LoginPage.java
@@ -0,0 +1,7 @@
+package com.qa.actitime;
+
+public class LoginPage {
+	public static void main(String[] args) {
+		System.out.println("Hi");
+	}
+}
---
+++ src/test/java/com/qa/test/RegestrationPage.java
@@ -0,0 +1,7 @@
+package com.qa.test;
+
+public class RegestrationPage {
+	public static void main(String[] args) {
+		System.out.println("Neelesh");
+	}
+}
